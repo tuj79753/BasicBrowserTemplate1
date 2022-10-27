9350fe325ed3cd4503a9e64100f31d1e4f09cd7c
Thien Le <89607263+tuj79753@users.noreply.github.com>
2022-10-27 12:18:42 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -0,0 +1,59 @@
+package edu.temple.basicbrowser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var urlEditText: EditText
+    lateinit var goButton: ImageButton
+    lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        urlEditText = findViewById(R.id.urlEditText)
+        goButton = findViewById(R.id.goButton)
+        webView = findViewById(R.id.webView)
+
+        webView.settings.javaScriptEnabled = true
+
+        var url : String
+
+        goButton.setOnClickListener{
+
+            url = urlEditText.text.toString()
+
+            var http : String = "https://"
+
+            var httpwww : String = "https://www."
+
+            if (url.substring(0, url.length) != http){
+
+                http += url
+
+                url = http
+            } else if(url.substring(0, url.length) != httpwww){
+
+                httpwww += url
+
+                url = httpwww
+
+            }
+
+            webView.loadUrl(url)
+
+        }
+
+
+        // Allow your browser to intercept hyperlink clicks
+        webView.webViewClient = WebViewClient()
+
+    }
+}
